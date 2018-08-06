4d50151ea4a0152189652a20a1ed0a39e6e34f1f
Shakeria Smith <shakerias2@gmail.com>
2018-08-06 01:53:13 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,9 +14,11 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  text.match(/^[A-Z].+[.?!"]$/) ? true : false
 
 end
 
 def valid_phone_number?(phone)
+  phone.match(/\(?\d{3}[) ]?\d{3}[- ]?\d{4}$/) ? true : false
 
 end
