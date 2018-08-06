065d2a292fb09d5610ac1bba363a4d4f37a58f97
Shakeria Smith <shakerias2@gmail.com>
2018-08-06 01:47:35 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,11 @@
 def starts_with_a_vowel?(word)
   word.match(/^[aeiouAEIOU]/) ? true : false
+
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
   text.scan(/un\w+ing\b/
+
 end
 
 def words_five_letters_long(text)
